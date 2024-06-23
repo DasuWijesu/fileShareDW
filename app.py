b05e9b875f293b87b331723293e930a3f385098b
DasuWijesu <127611557+DasuWijesu@users.noreply.github.com>
2024-06-23 17:26:19 +0530

    modified app.py & create and modified index.html

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+# Set the folder to store uploaded files
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file', filename=filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
