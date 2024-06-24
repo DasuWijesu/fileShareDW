b5ac46330f674ce8bf57502f4fb26d24f7352858
DasuWijesu <127611557+DasuWijesu@users.noreply.github.com>
2024-06-24 09:37:38 +0530

    create crud_app.py n crud_index.html and modified metadata.html n upload.html

---
+++ crud_app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, abort
+import os
+
+app = Flask(__name__)
+
+# Set the folder to store uploaded files
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+# Function to list files in the uploads folder
+def list_files():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return files
+
+@app.route('/')
+def index():
+    files = list_files()
+    return render_template('crud_index.html', files=files)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            return redirect(url_for('index'))
+    return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/delete/<filename>', methods=['POST'])
+def delete_file(filename):
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    return redirect(url_for('index'))
+
+@app.route('/metadata/<filename>')
+def view_metadata(filename):
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if not os.path.exists(filepath):
+        abort(404)
+    
+    metadata = {
+        'filename': filename,
+        'size': os.path.getsize(filepath)
+    }
+    return render_template('metadata.html', metadata=metadata)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
